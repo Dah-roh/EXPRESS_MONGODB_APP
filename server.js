4256e8cb51153cf4d9aff17f52ee3e7f6d1702d7
dizsart <darogadibia@gmail.com>
2020-06-20 12:15:05 +0100

    add the connectionString to the mongodb

--- server.js
+++ server.js
@@ -3,10 +3,17 @@ const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 const app = express();
 const port = 3000;
-
+const connectionString = 'mongodb+srv://Tisque:<sqh00part>@cluster0-skw6i.mongodb.net/<dbname>?retryWrites=true&w=majority';
 app.listen(port, function() {
     console.log(`listening on ${port}`)
 })
+
+MongoClient.connect(connectionString, {
+    useUnifiedTopology: true
+  }, (err, client) => {
+    if (err) return console.error(err)
+    console.log('Connected to Database')
+  })
 
 // app.get('/', function(req, res) {
 //     res.send('Hello World')
