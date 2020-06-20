52edf675b931b559dda9885228ed9c25f7eb0a13
dizsart <darogadibia@gmail.com>
2020-06-20 11:32:26 +0100

    add html file to the folder directory

--- server.js
+++ server.js
@@ -5,3 +5,12 @@ const port = 3000;
 app.listen(port, function() {
     console.log(`listening on ${port}`)
 })
+
+// app.get('/', function(req, res) {
+//     res.send('Hello World')
+// })
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname+'/index.html')
+})
