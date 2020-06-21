af52f39f8fc2a0f9c7f607d30e0e9c921b777adc
dizsart <darogadibia@gmail.com>
2020-06-21 15:25:01 +0100

    add dependencies to the project directory

--- server.js
+++ server.js
@@ -3,19 +3,33 @@ const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 const app = express();
 const port = 3000;
-const connectionString = 'mongodb+srv://Tisque:sqh00part@cluster0-skw6i.mongodb.net/CRUD_APP?retryWrites=true&w=majority';
-app.listen(port, function() {
-    console.log(`listening on ${port}`)
-})
+const connectionString = 'mongodb+srv://Tisque:sqh00part@cluster0-skw6i.mongodb.net/crud_app?retryWrites=true&w=majority';
+
 MongoClient.connect(connectionString, { useUnifiedTopology: true })
   .then(client => {
     console.log('Connected to Database')
     const db = client.db('crud_app')
     const crudCollection = db.collection('qoutes')
+    app.use(bodyParser.urlencoded({ extended: true }))
+
+// app.get('/', (req, res) => {
     
+// })
+    app.listen(port, function() {
+      console.log(`listening on ${port}`)
+  })
+app.get('/', (req, res) => {
+  crudCollection.find().toArray()
+    .then(results => {      
+    res.sendFile(__dirname+'/index.html')
+      console.log(results)
+    })
+    .catch(error => console.error(error))
+  })
   app.post('/quotes', (req, res) => {
     crudCollection.insertOne(req.body)
       .then(result => {
+        res.redirect('/')
         console.log(result)
       })
       .catch(error => console.error(error))
@@ -28,12 +42,3 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
 // })
 
 // Make sure you place body-parser before your CRUD handlers!
-app.use(bodyParser.urlencoded({ extended: true }))
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname+'/index.html')
-})
-
-app.post('/quotes', (req, res) => {
-    console.log(req.body)
-})
