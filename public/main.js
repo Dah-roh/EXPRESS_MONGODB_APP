2f5f0dba1b69befeda4a91793c895f11a654fbda
dizsart <darogadibia@gmail.com>
2020-06-21 15:57:04 +0100

    add the update function with a fetch request to the main.js file in the public folder

--- public/main.js
+++ public/main.js
@@ -1,7 +1,15 @@
-app.use(express.static('public'))
+app.use(express.static("public"));
 // main.js
-const update = document.querySelector('#update-button')
+const update = document.querySelector("#update-button");
 
-update.addEventListener('click', _ => {
-    // Send PUT Request here
-  })
+update.addEventListener("click", (_) => {
+  // Send PUT Request here
+  fetch("/quotes", {
+    method: "put",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      name: "Darth Vadar",
+      quote: "I find your lack of faith disturbing.",
+    }),
+  });
+});
